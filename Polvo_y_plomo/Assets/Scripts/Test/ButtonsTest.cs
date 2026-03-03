dc99a23373fd0c5b490fadf7e806387fecaf6cd1
Ángel Seijas <angelsei@ucm.es>
2026-03-03 15:10:03 +0100

    Creado el componente FadeColor para Miguel, que pueda hacer el respawn del jugador con la pantalla volviendose negra. Además he cambiado la escena del Test_InputManager y el componente de Test_Buttons para que se puedan ver en el ejecutable

--- Polvo_y_plomo/Assets/Scripts/Test/ButtonsTest.cs
+++ Polvo_y_plomo/Assets/Scripts/Test/ButtonsTest.cs
@@ -5,6 +5,7 @@
 // Proyectos 1 - Curso 2025-26
 //---------------------------------------------------------
 
+using TMPro;
 using UnityEngine;
 // Añadir aquí el resto de directivas using
 
@@ -22,6 +23,12 @@ public class ButtonsTest : MonoBehaviour
     // públicos y de inspector se nombren en formato PascalCase
     // (palabras con primera letra mayúscula, incluida la primera letra)
     // Ejemplo: MaxHealthPoints
+
+    /// <summary>
+    /// Escribirá el input recibido en este texto.
+    /// </summary>
+    [SerializeField]
+    private TextMeshProUGUI TextoDeInput;
 
     #endregion
 
@@ -49,6 +56,11 @@ public class ButtonsTest : MonoBehaviour
             Debug.Log("Se ha puesto el componente \"ButtonsTest\" en una escena sin InputManager. No funcionará.");
             Destroy(this);
         }
+        if (TextoDeInput == null)
+        {
+            Debug.Log("Se ha puesto el componente \"ButtonsTest\" sin asignarle TextoDeInput. No funcionará");
+            Destroy(this);
+        }
     }
     /// <summary>
     /// Se llama cada frame.
@@ -56,29 +68,32 @@ public class ButtonsTest : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (InputManager.Instance.FireIsPressed()) Debug.Log("Fire is pressed");
-        if (InputManager.Instance.FireWasPressedThisFrame()) Debug.Log("Fire was pressed this frame");
-        if (InputManager.Instance.FireWasReleasedThisFrame()) Debug.Log("Fire was released this frame");
+        if (InputManager.Instance.FireIsPressed()) TextoDeInput.text = "Fire is pressed";
+        if (InputManager.Instance.FireWasPressedThisFrame()) TextoDeInput.text = "Fire was pressed this frame";
+        if (InputManager.Instance.FireWasReleasedThisFrame()) TextoDeInput.text = "Fire was released this frame";
 
-        if (InputManager.Instance.MeleeIsPressed()) Debug.Log("Melee is pressed");
-        if (InputManager.Instance.MeleeWasPressedThisFrame()) Debug.Log("Melee was pressed this frame");
-        if (InputManager.Instance.MeleeWasReleasedThisFrame()) Debug.Log("Melee was released this frame");
+        if (InputManager.Instance.MeleeIsPressed()) TextoDeInput.text = "Melee is pressed";
+        if (InputManager.Instance.MeleeWasPressedThisFrame()) TextoDeInput.text = "Melee was pressed this frame";
+        if (InputManager.Instance.MeleeWasReleasedThisFrame()) TextoDeInput.text = "Melee was released this frame";
 
-        if (InputManager.Instance.ReloadIsPressed()) Debug.Log("Reload is pressed");
-        if (InputManager.Instance.ReloadWasPressedThisFrame()) Debug.Log("Reload was pressed this frame");
-        if (InputManager.Instance.ReloadWasReleasedThisFrame()) Debug.Log("Reload was released this frame");
+        if (InputManager.Instance.ReloadIsPressed()) TextoDeInput.text = "Reload is pressed";
+        if (InputManager.Instance.ReloadWasPressedThisFrame()) TextoDeInput.text = "Reload was pressed this frame";
+        if (InputManager.Instance.ReloadWasReleasedThisFrame()) TextoDeInput.text = "Reload was released this frame";
 
-        if (InputManager.Instance.HabilityIsPressed()) Debug.Log("Hability is pressed");
-        if (InputManager.Instance.HabilityWasPressedThisFrame()) Debug.Log("Hability was pressed this frame");
-        if (InputManager.Instance.HabilityWasReleasedThisFrame()) Debug.Log("Hability was released this frame");
+        if (InputManager.Instance.HabilityIsPressed()) TextoDeInput.text = "Hability is pressed";
+        if (InputManager.Instance.HabilityWasPressedThisFrame()) TextoDeInput.text = "Hability was pressed this frame";
+        if (InputManager.Instance.HabilityWasReleasedThisFrame()) TextoDeInput.text = "Hability was released this frame";
 
-        if (InputManager.Instance.RollIsPressed()) Debug.Log("Roll is pressed");
-        if (InputManager.Instance.RollWasPressedThisFrame()) Debug.Log("Roll was pressed this frame");
-        if (InputManager.Instance.RollWasReleasedThisFrame()) Debug.Log("Roll was released this frame");
+        if (InputManager.Instance.RollIsPressed()) TextoDeInput.text = "Roll is pressed";
+        if (InputManager.Instance.RollWasPressedThisFrame()) TextoDeInput.text = "Roll was pressed this frame";
+        if (InputManager.Instance.RollWasReleasedThisFrame()) TextoDeInput.text = "Roll was released this frame";
 
-        if (InputManager.Instance.ExitIsPressed()) Debug.Log("Exit is pressed");
-        if (InputManager.Instance.ExitWasPressedThisFrame()) Debug.Log("Exit was pressed this frame");
-        if (InputManager.Instance.ExitWasReleasedThisFrame()) Debug.Log("Exit was released this frame");
+        if (InputManager.Instance.ExitIsPressed()) TextoDeInput.text = "Exit is pressed";
+        if (InputManager.Instance.ExitWasPressedThisFrame()) TextoDeInput.text = "Exit was pressed this frame";
+        if (InputManager.Instance.ExitWasReleasedThisFrame()) TextoDeInput.text = "Exit was released this frame";
+
+        if (InputManager.Instance.MovementVector != Vector2.zero) TextoDeInput.text = "Move is being used and has a value of (" + InputManager.Instance.MovementVector.x + ", " + InputManager.Instance.MovementVector.y + ").";
+        if (InputManager.Instance.LookVector != Vector2.zero) TextoDeInput.text = "Look is being used and has a value of (" + InputManager.Instance.LookVector.x + ", " + InputManager.Instance.LookVector.y + ").";
     }
     #endregion
 
