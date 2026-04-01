d00aa735a7b50c4532835b64ed38f9ce21500e1a
CamiloSandovalSanchez <camilosa@ucm.es>
2026-04-01 18:02:05 +0200

    Enem anim prueba

---
+++ Polvo_y_plomo/Assets/Scripts/EnemySpawnAnim.cs
@@ -0,0 +1,129 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// CamiloSandovalSánchez
+// Polvo y plomo
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using System;
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class EnemySpawnAnim : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    [SerializeField]
+    private int SpawnAnimation;
+    [SerializeField]
+    private float SpawnDuration = 5f;
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    private ChasePlayer _chasePlayer;
+    private EnemyMeleeAttack _enemyMeleeAttack;
+    private float _spawnTime;
+    private Animator _animator;
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        if (_animator == null)
+        {
+            Debug.Log("Se ha puesto el componente EnemySpawnAnim sin el componente Animator");
+            Destroy(this);
+        }
+        _chasePlayer = GetComponent<ChasePlayer>();
+        if (_chasePlayer != null)
+        {
+            _chasePlayer.enabled = false;
+        }
+        _enemyMeleeAttack = GetComponent<EnemyMeleeAttack>();
+        if (_enemyMeleeAttack != null)
+        {
+            _enemyMeleeAttack.enabled = false;
+        }
+        //string[] anim = (SpawnAnimation.ToString()).Split();
+        _spawnTime = Time.time;
+        _animator.SetInteger("SpawnType", SpawnAnimation);
+    }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (Time.time - _spawnTime > SpawnDuration)
+        {
+            _animator.SetInteger("SpawnType", 0);
+            _chasePlayer = GetComponent<ChasePlayer>();
+            if (_chasePlayer != null)
+            {
+                _chasePlayer.Stunned(false);
+                _chasePlayer.enabled = true;
+            }
+            _enemyMeleeAttack = GetComponent<EnemyMeleeAttack>();
+            if (_enemyMeleeAttack != null)
+            {
+                _enemyMeleeAttack.enabled = true;
+            }
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class EnemySpawnAnim 
+// namespace
